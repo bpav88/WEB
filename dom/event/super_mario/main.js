96b3eb19570fb4487846481a4cee719c480f7c9f
BIT student <classroom@bgit.rs>
2018-11-07 11:39:07 +0100

    super mario done

--- dom/event/super_mario/main.js
+++ dom/event/super_mario/main.js
@@ -2,38 +2,34 @@ var $div = document.querySelector('div');
 var $img = document.querySelector('img');
 
 var pr;
+var x = 0;
+var toggle = false;
+var sound = new Audio('./sound/sound.mp3');
+
+function move() {
+    x -= 20;
+    $div.style.backgroundPosition = x + 'px';
+}
+
 document.addEventListener('keydown', function (event) {
-    if (event.keyCode === 39) {
+    if (event.keyCode === 39 && !toggle) {
         console.log('hello');
-        var x = 0;
-        pr = setInterval(function(){
-            x-=1;
-            $div.style.backgroundPosition = x +'px 0';
-        }, 100);
-        $img.removeAttribute('src');
+        pr = setInterval(move, 100);
+        // $img.removeAttribute('src');
         $img.setAttribute('src', './images/mario_running.gif');
+        sound.play();
+        toggle = true;
+
     }
 })
-
-// document.addEventListener('keypress', function (event) {
-//     if (event.keyCode === 39) {
-//         console.log('hello');
-//         var x = 0;
-//         pr = setInterval(function(){
-//             x-=1;
-//             $div.style.backgroundPosition = x +'px 0';
-//         }, 100);
-//         $img.removeAttribute('src');
-//         $img.setAttribute('src', './images/mario_running.gif');
-//     }
-// })
 
 document.addEventListener('keyup', function (event) {
     if (event.keyCode === 39) {
         console.log('blaaa');
         clearInterval(pr);
-        $img.removeAttribute('src');
-        $img.setAttribute('src', './images/mario.png');
-
+        // $img.removeAttribute('src');
+        $img.src = './images/mario.png';
+        sound.pause();
+        toggle = false;
     }
 })
